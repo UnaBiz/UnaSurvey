59a62a1ef91c8758e35a0f2b55a56c74995acdf0
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-03-28 15:42:24 +0800

    Data sent successfully

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const theThingsAPI = require("thethingsio-api");
-const client = theThingsAPI.createClient();
+const client = theThingsAPI.createSecureClient();
+const obj = {
+    "values": [{
+            "key": "temperature",
+            "value": 23,
+            "geo": {
+                "lat": 41.4121132,
+                "long": 2.2199454
+            }
+        }]
+};
+client.on('error', function (error) {
+    console.error(error.message, error.stack);
+});
 client.on('ready', function () {
-    client.thingRead('temperature', { limit: 1 }, function (error, data) {
-        console.log(error ? error : data);
+    client.thingWrite(obj, function (error, data) {
+        if (error) {
+            console.error(error.message, error.stack);
+            return error;
+        }
+        console.log({ data });
     });
+    /* client.thingRead('temperature', {limit:1}, function (error, data) {
+      if (error) {
+        console.error(error.message, error.stack);
+        return error;
+      }
+      console.log({ data });
+    }); */
 });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,9 +1,37 @@
 import * as theThingsAPI from 'thethingsio-api';
 
-const client = theThingsAPI.createClient();
+const client = theThingsAPI.createSecureClient();
+
+const obj = {
+  "values":
+    [{
+      "key": "temperature",
+      "value" : 23,
+      "geo" : {
+        "lat" : 41.4121132,
+        "long" : 2.2199454
+      }
+    }]
+};
+
+client.on('error',function(error){
+  console.error(error.message, error.stack);
+});
 
 client.on('ready', function() {
-  client.thingRead('temperature', {limit:1}, function (error, data) {
-    console.log(error ? error : data);
+  client.thingWrite(obj, function (error, data) {
+    if (error) {
+      console.error(error.message, error.stack);
+      return error;
+    }
+    console.log({ data });
   });
+
+  /* client.thingRead('temperature', {limit:1}, function (error, data) {
+    if (error) {
+      console.error(error.message, error.stack);
+      return error;
+    }
+    console.log({ data });
+  }); */
 });
