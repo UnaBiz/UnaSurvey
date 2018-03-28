1de440163fab4b2e507b0ee753a4030611548f5c
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-03-28 16:14:47 +0800

    Send data for 4 UnaBells to thethings.io

--- index.js
+++ index.js
@@ -5,31 +5,53 @@
 //  4D9F7C: Fair
 //  4DA0B6: Poor
 Object.defineProperty(exports, "__esModule", { value: true });
+//  We will also have 4 config files with a Thing Token each:
+//  unabell_4d9a51: Excellent
+//  unabell_4da240: Good Job
+//  unabell_4d9f7c: Fair
+//  unabell_4da0b6: Poor
+//  Each file contains: {"thingToken" : "..."}
+const allUnaBells = {
+    excellent: 'unabell_4d9a51',
+    goodjob: 'unabell_4da240',
+    fair: 'unabell_4d9f7c',
+    poor: 'unabell_4da0b6',
+};
+const allClients = {};
 const theThingsAPI = require("thethingsio-api");
-const client = theThingsAPI.createSecureClient();
+Object.keys(allUnaBells).forEach(tag => {
+    const unabellID = allUnaBells[tag];
+    const configFile = unabellID + '.json';
+    const client = theThingsAPI.createSecureClient(configFile);
+    allClients[tag] = client;
+    client.on('error', function (error) {
+        console.error(error.message, error.stack);
+    });
+    client.on('ready', function () {
+        const obj = { values: [{
+                    key: 'test',
+                    value: tag,
+                }] };
+        client.thingWrite(obj, function (error, data) {
+            if (error) {
+                console.error(error.message, error.stack);
+                return error;
+            }
+            console.log({ data });
+        });
+        /* client.thingRead('temperature', {limit:1}, function (error, data) {
+          if (error) {
+            console.error(error.message, error.stack);
+            return error;
+          }
+          console.log({ data });
+        }); */
+    });
+});
 const obj = {
     "values": [{
             "key": "status",
             "value": "button_pressed",
         }]
 };
-client.on('error', function (error) {
-    console.error(error.message, error.stack);
-});
-client.on('ready', function () {
-    client.thingWrite(obj, function (error, data) {
-        if (error) {
-            console.error(error.message, error.stack);
-            return error;
-        }
-        console.log({ data });
-    });
-    /* client.thingRead('temperature', {limit:1}, function (error, data) {
-      if (error) {
-        console.error(error.message, error.stack);
-        return error;
-      }
-      console.log({ data });
-    }); */
-});
 //# sourceMappingURL=index.js.map
