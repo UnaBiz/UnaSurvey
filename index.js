aca4f2122aedf270cf95436672a619316db76168
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-03-29 12:03:12 +0800

    Converted to promises

--- index.js
+++ index.js
@@ -17,40 +17,57 @@ const allUnaBells = {
     fair: 'unabell_4d9f7c',
     poor: 'unabell_4da0b6',
 };
-const allClientsByTag = {};
-const allClientsByID = {};
+const allClientsByTag = {}; //  Maps tag to the thethings client
+const allClientsByID = {}; //  Maps UnaBell ID to the thethings client
+const allTagsByID = {}; //  Maps UnaBell ID to the tag
 const theThingsAPI = require("thethingsio-api");
-function sendStatus(client, unabellID, tag) {
-    //  Send the UnaBell status to thethings cloud.
+function sendStatus(unabellID0) {
+    //  Send the UnaBell status to thethings cloud via thethings client. Returns a promise.
+    //  Standardize the ID.  If this is a Sigfox device ID like 4D9A51, convert to unabell_4d951.
+    if (!unabellID0)
+        return Promise.resolve('missing_id');
+    const unabellID = (unabellID0.indexOf('unabell_') === 0)
+        ? unabellID0
+        : 'unabell_' + unabellID0.toLowerCase();
+    const client = allClientsByID[unabellID];
+    if (!client)
+        return Promise.resolve('unknown_id');
+    const tag = allTagsByID[unabellID];
+    //  Status object to be sent.
     const obj = {
         values: [
             { key: 'status', value: 'button_pressed', geo: { lat: 1, long: 104 } },
             { key: 'tag', value: tag },
         ]
     };
-    client.thingWrite(obj, function (error, data) {
-        if (error) {
-            console.error(unabellID, tag, error.message, error.stack);
-            return error;
-        }
-        console.log(unabellID, tag, { data });
+    //  Send the data.
+    return new Promise((resolve, reject) => client.thingWrite(obj, (error, data) => error ? reject(error) : resolve(data)))
+        .then(result => {
+        console.log(unabellID, tag, { result });
+        return result;
+    })
+        .catch(error => {
+        console.error(unabellID, tag, error.message, error.stack);
+        throw error;
     });
 }
+exports.sendStatus = sendStatus;
 Object.keys(allUnaBells).forEach(tag => {
     //  Upon startup, open a connection for each UnaBell.
     const unabellID = allUnaBells[tag];
     const configFile = unabellID + '.json';
     const client = theThingsAPI.createSecureClient(configFile);
+    allTagsByID[unabellID] = tag;
     client.on('error', function (error) {
         console.error(unabellID, tag, error.message, error.stack);
     });
     client.on('ready', function () {
         //  Upon connecting, save the connection so we can send data later.
         allClientsByTag[tag] = client;
-        allClientsByID[tag] = client;
+        allClientsByID[unabellID] = client;
         //  For development: Send the test status upon connection.
         if (process.env.NODE_ENV !== 'production') {
-            sendStatus(client, unabellID, tag);
+            sendStatus(unabellID);
         }
     });
 });
--- index.ts
+++ index.ts
@@ -17,26 +17,43 @@ const allUnaBells = {
   fair: 'unabell_4d9f7c',
   poor: 'unabell_4da0b6',
 };
-const allClientsByTag = {};
-const allClientsByID = {};
+const allClientsByTag: {[tag: string]: object} = {};  //  Maps tag to the thethings client
+const allClientsByID: {[unabellID: string]: object} = {};  //  Maps UnaBell ID to the thethings client
+const allTagsByID: {[unabellID: string]: string} = {};  //  Maps UnaBell ID to the tag
 
 import * as theThingsAPI from 'thethingsio-api';
 
-function sendStatus(client: any, unabellID: any, tag: string) {
-  //  Send the UnaBell status to thethings cloud.
+export function sendStatus(unabellID0: string): Promise<any> {
+  //  Send the UnaBell status to thethings cloud via thethings client. Returns a promise.
+  //  Standardize the ID.  If this is a Sigfox device ID like 4D9A51, convert to unabell_4d951.
+  if (!unabellID0) return Promise.resolve('missing_id');
+  const unabellID = (unabellID0.indexOf('unabell_') === 0)
+    ? unabellID0
+    : 'unabell_' + unabellID0.toLowerCase();
+  const client = allClientsByID[unabellID];
+  if (!client) return Promise.resolve('unknown_id');
+  const tag = allTagsByID[unabellID];
+
+  //  Status object to be sent.
   const obj = {
     values: [
       {key: 'status', value: 'button_pressed', geo: { lat: 1, long: 104 }},
       {key: 'tag', value: tag},
     ]
   };
-  client.thingWrite(obj, function (error, data) {
-    if (error) {
+  //  Send the data.
+  return new Promise((resolve, reject) =>
+    client.thingWrite(obj, (error, data) =>
+      error ? reject(error) : resolve(data))
+  )
+    .then(result => {
+      console.log(unabellID, tag, { result });
+      return result;
+    })
+    .catch(error => {
       console.error(unabellID, tag, error.message, error.stack);
-      return error;
-    }
-    console.log(unabellID, tag, { data });
-  });
+      throw error;
+    });
 }
 
 Object.keys(allUnaBells).forEach(tag => {
@@ -44,6 +61,7 @@ Object.keys(allUnaBells).forEach(tag => {
   const unabellID = allUnaBells[tag];
   const configFile = unabellID + '.json';
   const client = theThingsAPI.createSecureClient(configFile);
+  allTagsByID[unabellID] = tag;
 
   client.on('error',function(error){
     console.error(unabellID, tag, error.message, error.stack);
@@ -52,11 +70,11 @@ Object.keys(allUnaBells).forEach(tag => {
   client.on('ready', function() {
     //  Upon connecting, save the connection so we can send data later.
     allClientsByTag[tag] = client;
-    allClientsByID[tag] = client;
+    allClientsByID[unabellID] = client;
 
     //  For development: Send the test status upon connection.
     if (process.env.NODE_ENV !== 'production') {
-      sendStatus(client, unabellID, tag);
+      sendStatus(unabellID);
     }
   });
 });
