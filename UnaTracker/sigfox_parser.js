49c6f900269c351c184947463ed7f570e123d629
lupyuen-unabiz <ly.lee@unabiz.com>
2018-04-08 22:02:43 +0800

    Send event to thethings via sigfox callback instead of JSON API

--- UnaTracker/sigfox_parser.js
+++ UnaTracker/sigfox_parser.js
@@ -1,11 +1,13 @@
-//  Decode the Sigfox message data into sensor-specific fields like WiFi MAC Address.  Called upon Sigfox callback.
+//  Decode the Sigfox message
 function main(params, callback){
-  // console.log(['*** sigfox_parser', new Date().toISOString(), JSON.stringify({ params }, null, 2)].join('-'.repeat(5))); ////
+  console.log(['*** sigfox_parser', new Date().toISOString(), JSON.stringify({ params }, null, 2)].join('-'.repeat(5))); ////
   const deviceId = params.deviceId;
   const data = params.data;
+  const custom = params.custom || {};
   //  data=2006e03f4924a3a0210564
   const messageType = data.substr(0,2);
   const dataType = data.substr(2,2);
+  const tag = custom.tag || '';
 
   //  If this message does not contain a wifi MAC Address (data type 06), then quit.
   if (dataType !== '06') return callback(null, [
@@ -13,6 +15,7 @@ function main(params, callback){
     {key: 'messageType', value: messageType },
     {key: 'dataType', value: dataType },
     {key: 'data', value: data },
+    {key: 'tag', value: tag },
   ]);
   const decodedDataType	='WiFi MAC Address';
   const macAddress	=data.substr(4,12);
@@ -23,7 +26,7 @@ function main(params, callback){
   const batteryLevel0	=(dataType2 === '05') ? data.substr(20, 2) : null;
   const batteryLevel	=(dataType2 === '05') ? parseInt(batteryLevel0, 16) : null;
 
-  const result = { deviceId, data, messageType, dataType, decodedDataType, macAddress, wifiRSSI0, wifiRSSI, dataType2, decodedDataType2, batteryLevel0, batteryLevel };
+  const result = { deviceId, data, messageType, dataType, decodedDataType, macAddress, wifiRSSI0, wifiRSSI, dataType2, decodedDataType2, batteryLevel0, batteryLevel, tag };
   const resultValue = Object.keys(result).map(key => ({
     key, value: result[key] }));
   return callback(null, resultValue);
