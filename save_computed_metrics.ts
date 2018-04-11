db19372f267f7e8e64cd83c1982ce04d8987a147
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-04-11 12:00:32 +0800

    Save computed metrics works OK for saving to 1 thing state

--- save_computed_metrics.ts
+++ save_computed_metrics.ts
@@ -1,39 +1,47 @@
 //  Save the metrics computed by Prometheus into the thing state for easy rendering on a dashboard
 var exports = {}; //  Needed for ES support.
 
-function saveThingState(thingToken: string, key: string, value, callback) {
-  const values = [
-    { key, value }
-  ];
-  return thethingsAPI.thingWrite(thingToken, { values }, (error, result) => {
-    if (error) {
-      console.error(error.message, error.stack);
-      return callback(null, error.message); // Don't propagate error to caller.
-    }
-    console.log(['*** saveThingState', new Date().toISOString(), JSON.stringify({ result, values }, null, 2)].join('-'.repeat(5)));
-    return callback(null, 'OK');
-  });
-}
-
-function recallThingState(thingToken: string, key: string, callback) {
-  return thethingsAPI.thingRead(thingToken, key, (error, result) => {
-    if (error) {
-      console.error(error.message, error.stack);
-      return callback(null, error.message); // Don't propagate error to caller.
-    }
-    console.log(['*** recallThingState', new Date().toISOString(), JSON.stringify({ result }, null, 2)].join('-'.repeat(5)));
-    return callback(null, result);
-  });
-}
-
 function main(params, callback) {
-  //  Request params contains a "tag" parameter e.g. "excellent". Take the last value, increment by 1 and update the value.
-  //  Send the new value to Prometheus.
+  //  Request params contains a list of alerts, which contain the updated metrics computed by Prometheus.
+  //  We save the computed metrics to the thing state.  The thing token is also provided in the params.
   console.log(['*** save_computed_metrics start', new Date().toISOString(), JSON.stringify({ params }, null, 2)].join('-'.repeat(5)));
   const thingToken = params.thingToken;
   const alerts = params.alerts;
-  console.log('*** alerts', alerts);
-  return callback(null, 'OK');
+
+  const values = [];  //  Will contain a list of {key, value}.
+  alerts.forEach(alert => {
+    /* alert contains {
+      "status": "firing",
+      "labels": {
+        "alertname": "button_presses_5m",
+        "instance": "poor",
+        "job": "job2"
+      },
+      "annotations": {
+        "description": "1",
+        "summary": "poor"
+      },
+      "startsAt": "2018-04-10T18:41:02.741584728Z",
+      "endsAt": "0001-01-01T00:00:00Z",
+      "generatorURL": "http://c186fd0ccdcb:8080/graph?g0.expr=button_presses_total+-+button_presses_total+offset+5m+%3E%3D+0&g0.tab=1"
+    }, */
+    const metric = alert.labels.alertname;  //  e.g. buttton_presses_5m
+    const instance = alert.labels.instance;  //  e.g. poor
+    const value = parseFloat(alert.annotations.description);  //  e.g. 1
+    const key = [metric, instance].join('_');  //  e.g. button_presses_5m_poor
+    values.push({key, value});  //  e.g. button_presses_5m_poor = 1
+    console.log('*** alert', {key, value});
+  });
+
+  return thethingsAPI.thingWrite(thingToken, { values }, (error, result) => {
+    if (error) {
+      console.error('*** save_computed_metrics error', error, error.message, error.stack, values);
+      return callback(null, error.message); // Don't propagate error to caller.
+    }
+    console.log(['*** save_computed_metrics OK', new Date().toISOString(), JSON.stringify({ result }, null, 2)].join('-'.repeat(5)));
+    return callback(null, result);
+  });
+
 
   /*
   const labelEntry = findParam(params, 'label');
@@ -80,5 +88,3 @@ function main(params, callback) {
   */
 
 }
-
-declare const thethingsAPI;
