0be0b135ee1bdde411c24bc460c8c1f706c789de
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-04-11 11:03:44 +0800

    Adding save_computed_metrics

---
+++ save_computed_metrics.js
@@ -0,0 +1,79 @@
+"use strict";
+//  Save the metrics computed by Prometheus into the thing state for easy rendering on a dashboard
+var exports = {}; //  Needed for ES support.
+function saveThingState(thingToken, key, value, callback) {
+    const values = [
+        { key, value }
+    ];
+    return thethingsAPI.thingWrite(thingToken, { values }, (error, result) => {
+        if (error) {
+            console.error(error.message, error.stack);
+            return callback(null, error.message); // Don't propagate error to caller.
+        }
+        console.log(['*** saveThingState', new Date().toISOString(), JSON.stringify({ result, values }, null, 2)].join('-'.repeat(5)));
+        return callback(null, 'OK');
+    });
+}
+function recallThingState(thingToken, key, callback) {
+    return thethingsAPI.thingRead(thingToken, key, (error, result) => {
+        if (error) {
+            console.error(error.message, error.stack);
+            return callback(null, error.message); // Don't propagate error to caller.
+        }
+        console.log(['*** recallThingState', new Date().toISOString(), JSON.stringify({ result }, null, 2)].join('-'.repeat(5)));
+        return callback(null, result);
+    });
+}
+function main(params, callback) {
+    //  Request params contains a "tag" parameter e.g. "excellent". Take the last value, increment by 1 and update the value.
+    //  Send the new value to Prometheus.
+    console.log(['*** save_computed_metrics start', new Date().toISOString(), JSON.stringify({ params }, null, 2)].join('-'.repeat(5)));
+    const thingToken = params.thingToken;
+    const alerts = params.alerts;
+    console.log('*** alerts', alerts);
+    return callback(null, 'OK');
+    /*
+    const labelEntry = findParam(params, 'label');
+    if (!labelEntry) return callback(new Error('missing_label'));
+    const label = labelEntry.value;
+    if (!label) return callback(new Error('missing_label_value'));
+  
+    //  Get the last count from thing state.
+    const key = 'count';
+    return recallThingState(thingToken, key, (error, result) => {
+      if (error) {
+        console.error(error.message, error.stack);
+        return callback(null, error.message); // Don't propagate error to caller.
+      }
+      //  Assume there is no previous count, init to 1.
+      let value = 1;
+      //  result=[{"key":"count","value":{"count":123...
+      if (result && result[0] && result[0].value) {
+        //  If there is a previous count, increment it.
+        value = result[0].value + 1;
+      }
+      //  Update the count in the thing state.
+      return saveThingState(thingToken, key, value, (error, result) => {
+        if (error) {
+          console.error(error.message, error.stack);
+          return callback(null, error.message); // Don't propagate error to caller.
+        }
+  
+        //  Send the updated count to Prometheus through cloud function send_time_series.
+        const sendParams = {
+          label,
+          count: value,
+        };
+        thethingsAPI.cloudFunction('send_time_series', sendParams, (error, result) => {
+          if (error) {
+            console.error('*** save_computed_metrics error', error.message, error.stack);
+            return callback(null, error.message); // Don't propagate error to caller.
+          }
+          console.log(['*** save_computed_metrics OK', new Date().toISOString(), JSON.stringify({ result, sendParams }, null, 2)].join('-'.repeat(5)));
+          return callback(null, result);
+        });
+      });
+    });
+    */
+}
+//# sourceMappingURL=save_computed_metrics.js.map
